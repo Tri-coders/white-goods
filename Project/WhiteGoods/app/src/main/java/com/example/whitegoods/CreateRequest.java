d8e66907124cde92910cf0252eb2fd05b0c1181a
sanket1305 <46529401+sanket1305@users.noreply.github.com>
2020-09-13 23:47:18 +0530

    Created Android Studio Project files

---
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/CreateRequest.java
@@ -0,0 +1,14 @@
+package com.example.whitegoods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_request);
+    }
+}
---
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/ViewEmployeeList.java
@@ -0,0 +1,14 @@
+package com.example.whitegoods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewEmployeeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_employee_list);
+    }
+}
