f2729745f5d6ce4115cb1296a05aa36c96fdf33c
AFIF <46858532+Afif79@users.noreply.github.com>
2020-09-15 20:05:17 +0530

    Reg page started

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/MainActivity.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.whitegoods;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reg();
+            }
+        });
+    }
+    public void reg(){
+        Intent i = new Intent(this, RegisterEmployee.class);
+        startActivity(i);
     }
 }
