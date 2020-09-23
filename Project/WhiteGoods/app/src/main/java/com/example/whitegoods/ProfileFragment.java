2469788d8f2c88093e04f4cdd8d994c48246cdb2
AFIF <46858532+Afif79@users.noreply.github.com>
2020-09-23 19:51:06 +0530

    Edit Profile Page Designed

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/ProfileFragment.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/ProfileFragment.java
@@ -1,21 +1,25 @@
 package com.example.whitegoods;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
-
+    ImageButton edit;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        edit = (ImageButton) root.findViewById(R.id.edit_profile);
+        Intent i = new Intent(getActivity(), EditProfile.class);
+        startActivity(i);
         return root;
     }
-}
+    }
