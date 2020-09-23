8986a0f7452f94fe2fc54c8a53a35dc491793f37
AFIF <46858532+Afif79@users.noreply.github.com>
2020-09-23 20:10:40 +0530

    Edit Profile Page Intent solved

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/ProfileFragment.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/ProfileFragment.java
@@ -18,8 +18,16 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
         edit = (ImageButton) root.findViewById(R.id.edit_profile);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit_profile();
+            }
+        });
+        return root;
+    }
+    public void edit_profile(){
         Intent i = new Intent(getActivity(), EditProfile.class);
         startActivity(i);
-        return root;
     }
     }
