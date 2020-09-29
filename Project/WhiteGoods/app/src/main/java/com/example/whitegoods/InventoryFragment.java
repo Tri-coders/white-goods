90ee5a2ea62b2580ff230aa3f2c8d2395a3b36fc
AFIF <46858532+Afif79@users.noreply.github.com>
2020-09-29 18:50:38 +0530

    inventory Detail page designed

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/InventoryFragment.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/InventoryFragment.java
@@ -1,5 +1,6 @@
 package com.example.whitegoods;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +8,29 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class InventoryFragment extends Fragment {
-
+    CardView fridge;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_inventory, container, false);
+        fridge = root.findViewById(R.id.fridge);
+        fridge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFridge();
+
+            }
+        });
         return root;
     }
+
+    public void openFridge(){
+        Intent i = new Intent(getActivity(), InventoryDetail.class);
+        startActivity(i);
+     }
 }
