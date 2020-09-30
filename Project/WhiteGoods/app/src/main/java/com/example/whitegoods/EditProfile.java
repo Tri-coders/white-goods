0f8a14b132320ccf4ec184b031660f48e88caff8
AFIF <46858532+Afif79@users.noreply.github.com>
2020-09-30 18:36:15 +0530

    Revert "bottom dialog modal added in edit profile"
    This reverts commit 296bc2dcb32550fb1cb8d082bfea4e88dbb9255b.

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/EditProfile.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/EditProfile.java
@@ -3,12 +3,6 @@ package com.example.whitegoods;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-
-import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -16,19 +10,5 @@ public class EditProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-
-        ImageButton buttonShow = findViewById(R.id.saveProfile);
-        buttonShow.setOnClickListener((View.OnClickListener) view -> {
-            BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
-                    EditProfile.this, R.style.BottomSheetDialogTheme
-            );
-
-            View bottomSheetView = LayoutInflater.from(getApplicationContext()).inflate(
-                    R.layout.layout_bottom_sheet,
-                    (LinearLayout)findViewById(R.id.bottomSheetContainer)
-            );
-            bottomSheetDialog.setContentView(bottomSheetView);
-            bottomSheetDialog.show();
-        });
     }
 }
