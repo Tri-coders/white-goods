8d9b518550df2dbfa953e98266c9cb18ff0dcd72
meetsanket24@gmail.com <meetsanket24@gmail.com>
2020-10-01 23:23:35 +0530

    Login code changed

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/MainActivity.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/MainActivity.java
@@ -27,8 +27,9 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private Button login;
-    private EditText email,password;
-    private static String URL_LOGIN = "http://128.199.30.114:9000/login";
+    private EditText email, password;
+    private static String server_url = "http://128.199.30.114:9000/login";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,39 +41,96 @@ public class MainActivity extends AppCompatActivity {
 
         login.setOnClickListener(view -> loginUser());
     }
+
     public void loginUser() {
 //        Intent i = new Intent(this, RegisterEmployee.class);
 //        startActivity(i);
-        login.setVisibility(View.GONE);
+        Log.i("sankey", "login clicked");
+//        login.setVisibility(View.GONE);
 
         final String email = this.email.getText().toString();
         final String password = this.password.getText().toString();
 
-        func(email,password);
-        login.setVisibility(View.VISIBLE);
+        func(email, password);
+//        login.setVisibility(View.VISIBLE);
     }
-    private void func(String email,String password)
-    {
-        RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
 
-        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL_LOGIN,
+    private void func(String email, String password) {
+        final JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("email", email);
+            jsonObject.put("password", password);
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        final String requestBody = jsonObject.toString();
+        Log.i("VolleyABC", requestBody);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.i("VolleyABC", "got response " + response);
+                Toast.makeText(MainActivity.this, "Logged IN", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                try {
+                    Log.i("VolleyABC", Integer.toString(error.networkResponse.statusCode));
+                    Toast.makeText(MainActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+
+                    error.printStackTrace();
+                }
+                catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Check Network", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }) {
+            //sending JSONObject String to server starts
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+        };
+        //sending JSONObject String to server ends
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest); // get response from server
+    }
+
+    private void func1(String email, String password) {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
                 response -> {
 
                     //Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
-                    if(response.equals("ok")) {
+                    if (response.equals("ok")) {
                         Intent i = new Intent(getApplicationContext(), RegisterEmployee.class);
                         startActivity(i);
-                    }else{
-                        Toast.makeText(getApplicationContext(),"Not Valid User!!",Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Not Valid User!!", Toast.LENGTH_LONG).show();
 
                     }
                 },
-                error -> Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show()){
+                error -> Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show()) {
             @Override
-            protected  Map<String, String> getParams() {
+            protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
-                params.put("email",email);
-                params.put("pass",password);
+                params.put("email", email);
+                params.put("pass", password);
                 return params;
             }
         };
