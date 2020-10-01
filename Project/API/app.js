1751ebbf3d62064a44b0f296eedf0dfddea3c06a
meetsanket24@gmail.com <meetsanket24@gmail.com>
2020-10-01 20:26:04 +0530

    API changed

--- Project/API/app.js
+++ Project/API/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors')
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
