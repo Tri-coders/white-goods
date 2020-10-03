32e147f057cea7e6f54d7e806dfc5385c7aba8c7
sanket1305 <46529401+sanket1305@users.noreply.github.com>
2020-10-03 17:03:28 +0530

    Added /register API for employee registration

--- Project/API/app.js
+++ Project/API/app.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+var nodemailer = require('nodemailer');
+
 var mysql = require('mysql')
 const bcrypt = require('bcrypt');
 
@@ -31,14 +33,14 @@ app.get('/', (req, res) => {
     var password = "Sanket@2000"
 
     var hash1 = bcrypt.hashSync(password, saltRounds);
-    var sql = "INSERT INTO login_details (user_id, password) VALUES ('1', '"+ hash1 +"');";
+    var sql = "INSERT INTO login_details (user_id, password) VALUES ('1', '" + hash1 + "');";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        if(result) {
+        if (result) {
             res.sendStatus(200);
         }
         else {
-            res.status(400).send({"error":"Username or Password is incorrect"})
+            res.status(400).send({ "error": "Username or Password is incorrect" })
         }
     });
 });
@@ -49,21 +51,145 @@ app.post('/login', (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
-    var sql = "SELECT login_details.password FROM login_details INNER JOIN user_details ON login_details.user_id = user_details.user_id WHERE user_details.email='"+email+"';";
+    var sql = "SELECT login_details.password FROM login_details INNER JOIN user_details ON login_details.user_id = user_details.user_id WHERE user_details.email='" + email + "';";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log(result)
-        if(result.length == 0) {
-            res.status(404).send({"error":"No user exists with this email"})
+        if (result.length == 0) {
+            res.status(404).send({ "error": "No user exists with this email" })
         }
-        else if(bcrypt.compareSync(password, result[0].password)) {
+        else if (bcrypt.compareSync(password, result[0].password)) {
             res.sendStatus(200);
         }
         else {
-            res.status(400).send({"error":"Username or Password is incorrect"})
+            res.status(400).send({ "error": "Username or Password is incorrect" })
         }
     });
 
+});
+
+//register API
+app.post('/register', (req, res) => {
+    console.log("register called")
+    //get al user details
+    var name = req.body.name;
+    var mobile = req.body.mobile;
+    var email = req.body.email;
+    var address = req.body.address;
+    var city = req.body.city;
+    var pin = req.body.pincode;
+    var demo = req.body.demo;
+    var install = req.body.install;
+    var inventory = req.body.inventory;
+    var upgrade = req.body.upgrade;
+
+    var fname = name.split(" ");
+    fname = fname[0]
+
+    //insert all user details into DB
+    var sql = "INSERT INTO user_details (name, address, city, pin, contact, email, role) VALUES ('" + name + "', '" + address + "', '" + city + "', '" + pin + "', '" + mobile + "', '" + email + "', '1');";
+    // console.log(sql);
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        if (result) {
+
+            //get user_id from DB to generate unique password using user_id and and name
+            var sql = "SELECT user_id FROM user_details WHERE email='" + email + "';";
+            con.query(sql, function (err, result) {
+                if (err) throw err;
+                if (result) {
+
+                    var user_id = result[0].user_id
+                    var password = fname + "@" + user_id;
+                    console.log("Latest Password", password);
+
+                    var hash1 = bcrypt.hashSync(password, saltRounds);
+                    var sql = "INSERT INTO login_details (user_id, password) VALUES (" + user_id + ", '" + hash1 + "');";
+
+                    con.query(sql, function (err, result) {
+                        if (err) throw err;
+                        if (result) {
+
+                            var transporter = nodemailer.createTransport({
+                                service: 'gmail',
+                                auth: {
+                                    user: 'sanketdeveloper00@gmail.com',
+                                    pass: 'Sanket@123'
+                                }
+                            });
+
+                            var mailOptions = {
+                                from: 'sanketdeveloper00@gmail.com',
+                                to: email,
+                                subject: 'Welcome to whitegoods',
+                                text: 'Your Password is ' + password
+                            };
+
+                            transporter.sendMail(mailOptions, function (error, info) {
+                                if (error) {
+                                    console.log(error);
+                                } else {
+                                    console.log('Email sent: ' + info.response);
+                                    var sql = "INSERT INTO employee_role (user_id, install, inventory, demo, upgrade) VALUES (" + user_id + ", '" + install + "', '" + inventory + "', '" + demo + "', '" + upgrade + "');";
+                                    con.query(sql, function (err, result) {
+                                        if (err) throw err;
+                                        if (result) {
+                                            res.sendStatus(200);
+                                        }
+                                        else {
+                                            res.status(400).send({ "error": "unable to insert password and roles of user in DB " })
+                                        }
+                                    });
+                                }
+                            });
+
+                        }
+                        else {
+                            res.status(400).send({ "error": "unable to insert password and roles of user in DB " })
+                        }
+                    });
+
+                }
+                else {
+                    res.status(400).send({ "error": "Unable to get user_id from DB for given email" })
+                }
+            });
+
+            var password = "123456789"
+
+            var hash1 = bcrypt.hashSync(password, saltRounds);
+            var sql = "INSERT INTO login_details (user_id, password) VALUES ('1', '" + hash1 + "');";
+        }
+        else {
+            res.status(400).send({ "error": "Unable to insert user details into DB" })
+        }
+    });
+    // res.send(200)
+});
+
+app.get('/email', (req, res) => {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'sanketdeveloper00@gmail.com',
+            pass: 'Sanket@123'
+        }
+    });
+
+    var mailOptions = {
+        from: 'meetsanket24@gmail.com',
+        to: 'sanketdeshmukh880@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 });
 
 //Port Listenings
