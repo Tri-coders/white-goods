683c1dd1a8597e6dba4d8ed80d51ed152e40921d
sanket1305 <meetsanket24@gmail.com>
2020-10-05 09:44:21 +0000

    DB update

--- Database/whitegood.sql
+++ Database/whitegood.sql
@@ -1,100 +1,140 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.31, for Linux (x86_64)
 --
--- Host: localhost
--- Generation Time: Oct 03, 2020 at 07:55 PM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: whitegood
+-- ------------------------------------------------------
+-- Server version	5.7.31-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `whitegood`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `billing`
 --
 
+DROP TABLE IF EXISTS `billing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `billing` (
-  `billing_id` int(10) NOT NULL,
+  `billing_id` int(10) NOT NULL AUTO_INCREMENT,
   `request_id` int(10) NOT NULL,
   `product_id` varchar(15) NOT NULL,
-  `quantity` int(5) NOT NULL
+  `quantity` int(5) NOT NULL,
+  PRIMARY KEY (`billing_id`),
+  KEY `request_id` (`request_id`),
+  CONSTRAINT `billing_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `billing`
+--
+
+LOCK TABLES `billing` WRITE;
+/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `employee_role`
 --
 
+DROP TABLE IF EXISTS `employee_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `employee_role` (
   `user_id` int(4) NOT NULL,
   `install` varchar(1) NOT NULL,
   `inventory` varchar(1) NOT NULL,
   `demo` varchar(1) NOT NULL,
-  `upgrade` varchar(1) NOT NULL
+  `upgrade` varchar(1) NOT NULL,
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `employee_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_details` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `employee_role`
+--
+
+LOCK TABLES `employee_role` WRITE;
+/*!40000 ALTER TABLE `employee_role` DISABLE KEYS */;
+INSERT INTO `employee_role` VALUES (10,'1','0','1','1'),(11,'0','0','0','1'),(9,'0','0','0','0');
+/*!40000 ALTER TABLE `employee_role` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `inventory`
 --
 
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `inventory` (
   `product_id` varchar(15) NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `brand` varchar(50) NOT NULL,
   `category` varchar(50) NOT NULL,
-  `description` text DEFAULT NULL,
+  `description` text,
   `price` int(10) NOT NULL,
-  `quantity` varchar(10) NOT NULL
+  `quantity` varchar(10) NOT NULL,
+  PRIMARY KEY (`product_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `login_details`
 --
 
+DROP TABLE IF EXISTS `login_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `login_details` (
   `user_id` int(4) NOT NULL,
-  `password` varchar(200) NOT NULL
+  `password` varchar(200) NOT NULL,
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `login_details_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_details` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `login_details`
 --
 
-INSERT INTO `login_details` (`user_id`, `password`) VALUES
-(8, '$2b$10$jwj/rz0sj7kw6LgFG7d6WeyWJiixq6qKcgQreznr1ShrlgtjcnQf.'),
-(9, '$2b$10$HYZ6BlJ8v63gDQUthubUx.MAj.3gkDNSUHg.7Rcr5pSFMjq./EgaO');
-
--- --------------------------------------------------------
+LOCK TABLES `login_details` WRITE;
+/*!40000 ALTER TABLE `login_details` DISABLE KEYS */;
+INSERT INTO `login_details` VALUES (8,'$2b$10$jwj/rz0sj7kw6LgFG7d6WeyWJiixq6qKcgQreznr1ShrlgtjcnQf.'),(9,'$2b$10$HYZ6BlJ8v63gDQUthubUx.MAj.3gkDNSUHg.7Rcr5pSFMjq./EgaO'),(10,'$2b$10$4v7up9pw.OAkoqfiSfZ6aOPbyIHhn3rIbf6tiviwT3E4CBSYgWJsW'),(9,'$2b$10$0fkbXizzv7UTE4gSlkJpUOetJOED.e4ljPy3tFztzf5T0YF.ytY7a'),(9,'$2b$10$Ab4OdmUcon5Lk6WQU6JxNuTNcwygBlqXTsditNqi7tmXTTemogX9O'),(11,'$2b$10$zet8wo1hfBA863cIvuLXjud.q61l9tbfomkYBsPxtJ6pS7yRGHFgO'),(9,'$2b$10$QlqzKYk2MoNuVEWWxktOWeNJyRJ9PR5fke92MLvDIPJastiyFgmU6'),(9,'$2b$10$cN4wa9pNYGhZaRJ3Z2ClJeUW0XHtUpapiOpmPSY6WkMnw0yU3.rcO');
+/*!40000 ALTER TABLE `login_details` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `request`
 --
 
+DROP TABLE IF EXISTS `request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `request` (
-  `request_id` int(10) NOT NULL,
+  `request_id` int(10) NOT NULL AUTO_INCREMENT,
   `user_id` int(4) NOT NULL,
   `title` varchar(20) NOT NULL,
-  `description` text DEFAULT NULL,
+  `description` text,
   `name` varchar(35) NOT NULL,
   `address` varchar(75) NOT NULL,
   `city` varchar(15) NOT NULL,
@@ -104,29 +144,56 @@ CREATE TABLE `request` (
   `date` varchar(10) NOT NULL,
   `time` varchar(8) NOT NULL,
   `status` int(11) NOT NULL,
-  `otp` int(6) NOT NULL
+  `otp` int(6) NOT NULL,
+  PRIMARY KEY (`request_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `request_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_details` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `request`
+--
+
+LOCK TABLES `request` WRITE;
+/*!40000 ALTER TABLE `request` DISABLE KEYS */;
+/*!40000 ALTER TABLE `request` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `sells`
 --
 
+DROP TABLE IF EXISTS `sells`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sells` (
   `request_id` int(10) NOT NULL,
   `product_id` varchar(15) NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  KEY `request_id` (`request_id`),
+  CONSTRAINT `sells_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `sells`
+--
+
+LOCK TABLES `sells` WRITE;
+/*!40000 ALTER TABLE `sells` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sells` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user_details`
 --
 
+DROP TABLE IF EXISTS `user_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_details` (
-  `user_id` int(4) NOT NULL,
+  `user_id` int(4) NOT NULL AUTO_INCREMENT,
   `name` varchar(35) NOT NULL,
   `address` varchar(75) NOT NULL,
   `city` varchar(15) NOT NULL,
@@ -134,122 +201,29 @@ CREATE TABLE `user_details` (
   `contact` varchar(10) NOT NULL,
   `email` varchar(50) NOT NULL,
   `role` int(1) NOT NULL,
-  `image` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image` varchar(50) DEFAULT NULL,
+  `is_deleted` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `user_details`
 --
 
-INSERT INTO `user_details` (`user_id`, `name`, `address`, `city`, `pin`, `contact`, `email`, `role`, `image`) VALUES
-(8, 'Admin', 'Automobile Premier Road, Kurla (West)', 'Mumbai', '400070', '9702717188', 'sanketdeveloper00@gmail.com', 0, NULL),
-(9, 'Sameer SIngh', 'Khindipada, Bhandup (West)', 'Mumbai', '400078', '9082171403', 'sanketdeshmukh880@gmail.com', 1, NULL);
+LOCK TABLES `user_details` WRITE;
+/*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
+INSERT INTO `user_details` VALUES (8,'Admin','Automobile Premier Road, Kurla (West)','Mumbai','400070','9702717188','sanketdeveloper00@gmail.com',0,NULL,'0'),(9,'Sameer SIngh','Khindipada, Bhandup (West)','Mumbai','400078','9082171403','sanketdeshmukh880@gmail.com',1,NULL,'0'),(10,'Sanket Deshmukh','2, Jatadhari Chawl, New Sanjay Niwas, Bhandup (West)','Mumbai','400078','9702717188','meetsanket24@gmail.com',2,NULL,'0'),(11,'Prabodh','I.U.D.P. colony washim ','Washim','444505','8208023919','prabodh.shewalkar@gmail.com',2,NULL,'0');
+/*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `billing`
---
-ALTER TABLE `billing`
-  ADD PRIMARY KEY (`billing_id`),
-  ADD KEY `request_id` (`request_id`);
-
---
--- Indexes for table `employee_role`
---
-ALTER TABLE `employee_role`
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `inventory`
---
-ALTER TABLE `inventory`
-  ADD PRIMARY KEY (`product_id`);
-
---
--- Indexes for table `login_details`
---
-ALTER TABLE `login_details`
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `request`
---
-ALTER TABLE `request`
-  ADD PRIMARY KEY (`request_id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `sells`
---
-ALTER TABLE `sells`
-  ADD KEY `request_id` (`request_id`);
-
---
--- Indexes for table `user_details`
---
-ALTER TABLE `user_details`
-  ADD PRIMARY KEY (`user_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `billing`
---
-ALTER TABLE `billing`
-  MODIFY `billing_id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `request`
---
-ALTER TABLE `request`
-  MODIFY `request_id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user_details`
---
-ALTER TABLE `user_details`
-  MODIFY `user_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `billing`
---
-ALTER TABLE `billing`
-  ADD CONSTRAINT `billing_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`);
-
---
--- Constraints for table `employee_role`
---
-ALTER TABLE `employee_role`
-  ADD CONSTRAINT `employee_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_details` (`user_id`);
-
---
--- Constraints for table `login_details`
---
-ALTER TABLE `login_details`
-  ADD CONSTRAINT `login_details_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_details` (`user_id`);
-
---
--- Constraints for table `request`
---
-ALTER TABLE `request`
-  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_details` (`user_id`);
-
---
--- Constraints for table `sells`
---
-ALTER TABLE `sells`
-  ADD CONSTRAINT `sells_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-05  9:43:57
