43408114e2f0ee9cd31d1b1336c0ee93004a7092
meetsanket24@gmail.com <meetsanket24@gmail.com>
2020-11-18 22:59:26 +0530

    databse updated

---
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/AdminViewEmployeeProfile.java
@@ -0,0 +1,13 @@
+package com.example.whitegoods;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminViewEmployeeProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view_employee_profile);
+    }
+}
