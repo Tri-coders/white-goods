2da0b5ed525cfa22d701a88413378e34eed2d59f
meetsanket24@gmail.com <meetsanket24@gmail.com>
2020-11-22 11:35:53 +0530

    is_deleted condition added in get_employee API

--- Project/API/app.js
+++ Project/API/app.js
@@ -200,7 +200,7 @@ app.get('/email', (req, res) => {
 });
 
 app.post('/get_employee', (req, res) => {
-    var sql = "SELECT * FROM user_details as u INNER JOIN `employee_role` as e ON e.user_id=u.user_id;"
+    var sql = "SELECT * FROM user_details as u INNER JOIN `employee_role` as e ON e.user_id=u.user_id AND NOT u.is_deleted = '1';"
     // var sql = "SELECT * FROM user_details WHERE NOT role = '0' AND NOT is_deleted = '1'";
     con.query(sql, function (err, result) {
         if (err) throw err;
