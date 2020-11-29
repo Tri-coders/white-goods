249a680bac3e7d9f0b8ed903f83d3ac46e8af229
AFIF <46858532+Afif79@users.noreply.github.com>
2020-11-29 18:21:55 +0530

    create request page designed and tiny issues fixed

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/RequestFragment.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/RequestFragment.java
@@ -1,5 +1,6 @@
 package com.example.whitegoods;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,12 +10,28 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class RequestFragment extends Fragment {
-    @Nullable
-    @Override
+    View root;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_request, container, false);
+        root = inflater.inflate(R.layout.fragment_request, container, false);
+
+        floatingActionButtonCall();
+
         return root;
     }
+
+    private void floatingActionButtonCall(){
+
+        FloatingActionButton newReq = root.findViewById(R.id.add_req);
+        newReq.setOnClickListener(view -> {
+            Intent i = new Intent(getActivity(), CreateRequest.class);
+            startActivity(i);
+        });
+
+    }
+
 }
