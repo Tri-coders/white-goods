7ae2ac89f57a4cb758ea38d1827e7fc1f36ea369
AFIF <46858532+Afif79@users.noreply.github.com>
2020-12-01 15:35:57 +0530

    CreateRequest.java and RegisterEmployee.java forms design edited

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/CreateRequest.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/CreateRequest.java
@@ -3,12 +3,21 @@ package com.example.whitegoods;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class CreateRequest extends AppCompatActivity {
+    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_request);
+        backButton();
     }
+
+    private void backButton(){
+        backButton = findViewById(R.id.back);
+        backButton.setOnClickListener(view -> finish());
+    }
+
 }
