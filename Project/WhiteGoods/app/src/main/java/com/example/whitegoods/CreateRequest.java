cb9fe8506a81c745ec6522ff2db7d6bd643a3bd7
Prabodh <prabodh.shewalkar@gmail.com>
2020-12-03 11:08:18 +0530

    Create Request Bug fix

--- Project/WhiteGoods/app/src/main/java/com/example/whitegoods/CreateRequest.java
+++ Project/WhiteGoods/app/src/main/java/com/example/whitegoods/CreateRequest.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -115,82 +116,114 @@ public class CreateRequest extends AppCompatActivity {
         progressBar.setVisibility(View.VISIBLE);
 
         String user_id = this.user_id;
-        String titleTxt= this.title.getText().toString();
-        String description = this.reqDescription.getText().toString();
-        String name = this.name.getText().toString();
-        String addr = this.addr.getText().toString();
-        String city = this.city.getText().toString();
-        String pincode = this.pincode.getText().toString();
-        String phone = this.phone.getText().toString();
-        String email = this.email.getText().toString();
-        String time = this.time.getText().toString();
-        int serviceCharge = Integer.parseInt(this.serviceCharge.getText().toString());
-        int itemCost = Integer.parseInt(this.cost.getText().toString());
-        String discount = this.discount.getText().toString();
-        String dateText = this.dateText.getText().toString();
-        String totalCost = "Rs. "+String.valueOf((itemCost+serviceCharge) - ((itemCost+serviceCharge)*Double.parseDouble(discount)/100));
+        String titleTxt= String.valueOf(this.title.getText());
+        String description = String.valueOf(this.reqDescription.getText());
+        String name = String.valueOf(this.name.getText());
+        String addr = String.valueOf(this.addr.getText());
+        String city = String.valueOf(this.city.getText());
+        String pincode = String.valueOf(this.pincode.getText());
+        String phone = String.valueOf(this.phone.getText());
+        String email = String.valueOf(this.email.getText());
+        String time = String.valueOf(this.time.getText());
+        if(TextUtils.isEmpty(this.serviceCharge.getText())){
+            Toast.makeText(CreateRequest.this,"Service Charge Empty",Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.INVISIBLE);
+            return;
+        }
+        int serviceCharge = Integer.parseInt(String.valueOf((this.serviceCharge.getText())));
+
+        if(TextUtils.isEmpty(this.cost.getText())){
+            Toast.makeText(CreateRequest.this,"Cost Field Empty",Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.INVISIBLE);
+            return;
+        }
+        int itemCost = Integer.parseInt(String.valueOf(this.cost.getText()));
+
+        if(TextUtils.isEmpty(this.discount.getText())){
+            Toast.makeText(CreateRequest.this,"Discount Field Empty",Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.INVISIBLE);
+            return;
+        }
+        double discount = Double.parseDouble(String.valueOf(this.discount.getText()));
+
+        String dateText = String.valueOf(this.dateText.getText());
+        String totalCost = "Rs. "+ ((itemCost + serviceCharge) - ((itemCost + serviceCharge) * discount / 100));
+        Log.d("Totalcost", "TotalCost : "+totalCost);
+
 
         if(titleTxt.length()>0){
             if(description.length()>0){
                 if(name.length()>0){
                     if(addr.length()>0){
                         if(city.length()>0){
-                            if(pincode.length()>0){
+                            if(pincode.length()==6){
                                 if(email.length()>0 && email.matches(emailPattern)){
                                     String[] a = time.split(":",2);
                                     if(a.length!=2){
-                                        progressBar.setVisibility(View.GONE);
+                                        Toast.makeText(CreateRequest.this,"Time Not Valid",Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.INVISIBLE);
                                         return;
                                     }
                                     int hr = Integer.parseInt(a[0]);
                                     int min = Integer.parseInt(a[1]);
                                     if(hr>=0 && hr<=23 && min>=0 && min<=59){
                                         if(dateText.length()>0){
-                                            if(phone.length()>0){
+                                            if(phone.length()==10){
                                                 fun(user_id,titleTxt,description,name,addr,city,pincode,email,time,dateText,phone,serviceCharge,itemCost,discount,totalCost);
                                             }else{
-                                                progressBar.setVisibility(View.GONE);
+                                                Toast.makeText(CreateRequest.this,"Invalid Contact",Toast.LENGTH_SHORT).show();
+                                                progressBar.setVisibility(View.INVISIBLE);
                                                 return;
                                             }
                                         }else{
-                                            progressBar.setVisibility(View.GONE);
+                                            Toast.makeText(CreateRequest.this,"Invalid Date",Toast.LENGTH_SHORT).show();
+                                            progressBar.setVisibility(View.INVISIBLE);
                                             return;
                                         }
                                     }else{
-                                        progressBar.setVisibility(View.GONE);
+                                        Toast.makeText(CreateRequest.this,"Invalid Time",Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.INVISIBLE);
                                         return;
                                     }
                                 }else{
-                                    progressBar.setVisibility(View.GONE);
+                                    Toast.makeText(CreateRequest.this,"Invalid Email",Toast.LENGTH_SHORT).show();
+                                    progressBar.setVisibility(View.INVISIBLE);
                                     return;
                                 }
                             }else{
-                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(CreateRequest.this,"Inavid Pincode",Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.INVISIBLE);
                                 return;
                             }
                         }else{
-                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(CreateRequest.this,"Invalid City",Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
                             return;
                         }
                     }else{
-                        progressBar.setVisibility(View.GONE);
+                        Toast.makeText(CreateRequest.this,"Invalid Address",Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.INVISIBLE);
                         return;
                     }
                 }else{
-                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(CreateRequest.this,"Name field is Empty",Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
                     return;
                 }
             }else{
-                progressBar.setVisibility(View.GONE);
+                Toast.makeText(CreateRequest.this,"Description Field is Empty",Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
                 return;
             }
         }else{
-            progressBar.setVisibility(View.GONE);
+            Toast.makeText(CreateRequest.this,"Title field is Empty",Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.INVISIBLE);
             return;
         }
     }
 
-    private void fun(String user_id, String titleTxt, String description, String name, String addr, String city, String pincode, String email, String time, String dateText, String phone, int serviceCharge, int itemCost, String discount, String totalCost) {
+    private void fun(String user_id, String titleTxt, String description, String name, String addr, String city, String pincode, String email, String time, String dateText, String phone, int serviceCharge, int itemCost, double discount, String totalCost) {
+        String discount1 = String.valueOf(discount);
         Intent intent = new Intent(getApplicationContext(), SelectEmployee.class);
         intent.putExtra("user_id", user_id);
         intent.putExtra("title", titleTxt);
@@ -205,13 +238,13 @@ public class CreateRequest extends AppCompatActivity {
         intent.putExtra("phone", phone);
         intent.putExtra("serviceCharge", serviceCharge);
         intent.putExtra("itemCost", itemCost);
-        intent.putExtra("discount", discount);
+        intent.putExtra("discount", discount1);
         intent.putExtra("totalCost", totalCost);
         startActivity(intent);
 
     }
     //select_employee
-    
+
 
     private void backButton(){
         backButton = findViewById(R.id.back);
